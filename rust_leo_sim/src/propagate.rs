959cb07b57f2a95a4aa056288bd3b8a3ec4b1077
JNuevo-LS <jorgenuevo04@gmail.com>
2025-02-19 12:39:08 -0500

    optimized reading and propagation + improved readability

--- rust_leo_sim/src/propagate.rs
+++ rust_leo_sim/src/propagate.rs
@@ -1,15 +1,28 @@
 use std::error::Error;
 use sgp4::{self, Constants, Elements};
 use serde_json;
-use crate::read_csv::Satellite;
+use crate::satellite::OrbitalInstance;
 
-pub fn propagate_elements(satellite:&Satellite) -> Result<(), Box<dyn Error>> {
-    let _mu: f64 = 398600.4418;
+pub fn propagate_elements(instance: &OrbitalInstance, time:u32 ) -> Result<(), Box<dyn Error>> {
+    // let _mu: f64 = 398600.4418;
 
+    let elements = format_elements(instance)?;
+    let constants: Constants = sgp4::Constants::from_elements(&elements)?;
+
+    for hours in 0..time {
+        println!("t = {} min", hours * 60);
+        let prediction = constants.propagate(sgp4::MinutesSinceEpoch((hours * 60) as f64))?;
+        println!("    r = {:?} km", prediction.position);
+        println!("    ṙ = {:?} km.s⁻¹", prediction.velocity);
+    }
+    Ok(())
+}
+
+fn format_elements(instance: &OrbitalInstance) -> Result<Elements, Box <dyn Error>>{
     let elements: String = format!(
         r#"{{
-            "OBJECT_NAME": {},
-            "OBJECT_ID": "{}",
+            "OBJECT_NAME": "a",
+            "OBJECT_ID": "b",
             "EPOCH": "{}",
             "MEAN_MOTION": {},
             "ECCENTRICITY": {},
@@ -18,37 +31,26 @@ pub fn propagate_elements(satellite:&Satellite) -> Result<(), Box<dyn Error>> {
             "ARG_OF_PERICENTER": {},
             "MEAN_ANOMALY": {},
             "EPHEMERIS_TYPE": 0,
-            "CLASSIFICATION_TYPE": "{}",
-            "NORAD_CAT_ID": {},
+            "CLASSIFICATION_TYPE": "U",
+            "NORAD_CAT_ID": c,
             "ELEMENT_SET_NO": 999,
             "REV_AT_EPOCH": 9999,
             "BSTAR": {},
             "MEAN_MOTION_DOT": {},
             "MEAN_MOTION_DDOT": {}
         }}"#,
-        satellite.name,
-        satellite.international_designator,
-        satellite.epoch,
-        satellite.mean_motion,
-        satellite.eccentricity,
-        satellite.inclination,
-        satellite.raan,
-        satellite.perigee,
-        satellite.mean_anomaly,
-        satellite.security_class,
-        satellite.catalog_number,
-        satellite.drag,
-        satellite.first_time_derivative,
-        satellite.second_time_derivative
+        instance.epoch,
+        instance.mean_motion,
+        instance.eccentricity,
+        instance.inclination,
+        instance.raan,
+        instance.perigee,
+        instance.mean_anomaly,
+        instance.drag,
+        instance.first_time_derivative,
+        instance.second_time_derivative
     );
-    let json: Elements  = serde_json::from_str(elements.as_str())?;
-    let constants: Constants = sgp4::Constants::from_elements(&json)?;
-
-    for hours in 0..24 {
-        // println!("t = {} min", hours * 60);
-        let _prediction = constants.propagate(sgp4::MinutesSinceEpoch((hours * 60) as f64))?;
-        // println!("    r = {:?} km", prediction.position);
-        // println!("    ṙ = {:?} km.s⁻¹", prediction.velocity);
-    }
-    Ok(())
+    
+    let elements: Elements  = serde_json::from_str(elements.as_str())?;
+    Ok(elements)
 }
